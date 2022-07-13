4e6b10ffab8de321fae8ae2c7087be922c362cd1
swethadevarinti <108727555+swethadevarinti@users.noreply.github.com>
2022-07-13 22:04:04 +0530

    Add files via upload

---
+++ Design_patterns/Interfacesegrigation.java
@@ -0,0 +1,30 @@
+
+interface Resturent{
+	void vegMeals() ;
+	void nonVegMeals();
+}
+
+class udipi implements Resturent{
+
+	@Override
+	public void vegMeals() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nonVegMeals() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+public class Interfacesegrigation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
